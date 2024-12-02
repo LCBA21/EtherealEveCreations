695e87012bc11eb227aab90e13b9235822e44ae7
LCBA21 <lesiban56@gmail.com>
2024-12-02 15:34:10 +0200

    #	modified:   etherealevecreations/src/main/java/com/plusplus/etherealevecreations/controller/ImageController.java #	modified:   etherealevecreations/src/main/java/com/plusplus/etherealevecreations/service/image/ImageServiceImpl.java #	modified:   etherealevecreations/src/main/java/com/plusplus/etherealevecreations/service/product/ProductService.java #	modified:   etherealevecreations/src/main/resources/application.properties #

--- etherealevecreations/src/main/java/com/plusplus/etherealevecreations/entity/Image.java
+++ etherealevecreations/src/main/java/com/plusplus/etherealevecreations/entity/Image.java
@@ -1,13 +1,16 @@
 package com.plusplus.etherealevecreations.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Blob;
-
+import java.sql.SQLException;
 @Entity
 @Data
 @AllArgsConstructor
@@ -22,17 +25,33 @@ public class Image {
     private String fileType;
 
     @Lob
+    @JsonIgnore // Exclude this field from serialization
     private Blob image;
+
     private String downloadUrl;
+
     @ManyToOne
     @JoinColumn(name = "product_id")
+    @JsonIgnoreProperties({"images"}) // Avoid circular references
     private Product product;
 
-    public Image(String fileName,String fileType,Blob image,String downloadUrl) {
-        this.fileName=fileName;
-        this.fileType=fileType;
-        this.image=image;
-        this.downloadUrl=downloadUrl;
+    public Image(String fileName, String fileType, Blob image, String downloadUrl) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.image = image;
+        this.downloadUrl = downloadUrl;
     }
 
+    // Add this method for serialization
+    @JsonProperty("imageData") // Include a new field in the response
+    public byte[] getImageData() {
+        if (image != null) {
+            try {
+                return image.getBytes(1, (int) image.length());
+            } catch (SQLException e) {
+                throw new RuntimeException("Error reading image Blob", e);
+            }
+        }
+        return null;
+    }
 }
